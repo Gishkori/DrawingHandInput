c848310759ceed84ccd490f43ef9624aa64b7eff
Ali Asghar Gishkori <gishkori@hotmail.com>
2023-10-26 21:41:04 +1100

    Initial Commit

---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,18 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Ali Asghar Khan Gishkori on 26/10/2023.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import CoreGraphics
+
+
+struct HexagonParameters {
+    let line: CGPoint
+    let curve: CGPoint
+    let control: CGPoint
+    
+    
+}
