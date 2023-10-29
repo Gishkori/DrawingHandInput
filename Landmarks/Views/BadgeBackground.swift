00214faf480720e4094bdc9eca68c14da14bd0b4
Ali Asghar Gishkori <gishkori@hotmail.com>
2023-10-29 19:34:34 +1100

    created a shape of diamond in the app

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,19 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Ali Asghar Khan Gishkori on 29/10/2023.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
